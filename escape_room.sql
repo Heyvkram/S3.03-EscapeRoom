ac328e0e8751b029f0dc7f69df4d0ce416162503
Heyvkram <mrc.luq.com>
2025-03-26 12:48:23 +0100

    SQL corrections

--- escape_room.sql
+++ escape_room.sql
@@ -2,9 +2,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS escape_room_schema;
-CREATE DATABASE escape_room_schema;
-USE escape_room_schema;
+DROP DATABASE IF EXISTS escape_room;
+CREATE DATABASE escape_room;
+USE escape_room;
 
 CREATE TABLE `clues` (
   `clue_id` int(11) NOT NULL,
@@ -14,7 +14,8 @@ CREATE TABLE `clues` (
   `clue_game_phase` varchar(100) NOT NULL,
   `clue_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `clue_price` int(11) NOT NULL,
-  `clue_value` int(11) NOT NULL
+  `clue_value` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `decoration_items` (
@@ -26,13 +27,16 @@ CREATE TABLE `decoration_items` (
   `decoration_item_clue_valor` int(11) DEFAULT NULL,
   `decoration_item_img` varchar(100) DEFAULT NULL,
   `decoration_item_creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `decoration_item_modification_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `decoration_item_modification_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `clue_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `game_sessions` (
   `game_id` int(11) NOT NULL,
   `room_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `notifications` (
@@ -40,8 +44,7 @@ CREATE TABLE `notifications` (
   `notification_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `notification_type` enum('Generic','Personal') DEFAULT NULL,
   `notification_description` varchar(200) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `payments` (
@@ -79,21 +82,23 @@ CREATE TABLE `users` (
 
 
 ALTER TABLE `clues`
-  ADD PRIMARY KEY (`clue_id`);
+  ADD PRIMARY KEY (`clue_id`),
+  ADD KEY `fk_clues_room` (`room_id`);
 
 ALTER TABLE `decoration_items`
   ADD PRIMARY KEY (`decoration_item_id`),
-  ADD UNIQUE KEY `name` (`decoration_item_name`);
+  ADD UNIQUE KEY `clue_id` (`clue_id`),
+  ADD KEY `fk_room_decoration` (`room_id`);
 
 ALTER TABLE `game_sessions`
   ADD PRIMARY KEY (`game_id`),
   ADD KEY `fk_game_user` (`user_id`),
-  ADD KEY `fk_game_room` (`room_id`);
+  ADD KEY `fk_game_room` (`room_id`),
+  ADD KEY `fk_game_payment` (`payment_id`);
 
 ALTER TABLE `notifications`
   ADD PRIMARY KEY (`notification_id`),
-  ADD KEY `fk_notification_user` (`user_id`),
-  ADD KEY `fk_notification_room` (`room_id`);
+  ADD KEY `fk_notification_user` (`user_id`);
 
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`payment_id`),
@@ -113,14 +118,24 @@ ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 
+ALTER TABLE `clues`
+  ADD CONSTRAINT `fk_clues_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
+
+ALTER TABLE `decoration_items`
+  ADD CONSTRAINT `fk_room_decoration` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
+
 ALTER TABLE `game_sessions`
+  ADD CONSTRAINT `fk_game_payment` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`payment_id`),
   ADD CONSTRAINT `fk_game_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`),
   ADD CONSTRAINT `fk_game_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 ALTER TABLE `notifications`
-  ADD CONSTRAINT `fk_notification_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`),
   ADD CONSTRAINT `fk_notification_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 ALTER TABLE `payments`
   ADD CONSTRAINT `fk_payment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
