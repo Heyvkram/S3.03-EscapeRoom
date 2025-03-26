ae1ce12262c67cb0c9d8dca76d296af9eed02cc9
Heyvkram <mrc.luq.com>
2025-03-26 13:17:44 +0100

    SQL corrections

--- escape_room.sql
+++ escape_room.sql
@@ -7,7 +7,7 @@ CREATE DATABASE escape_room;
 USE escape_room;
 
 CREATE TABLE `clues` (
-  `clue_id` int(11) NOT NULL,
+  `clue_id` int(11) UNIQUE NOT NULL,
   `clue_name` varchar(100) NOT NULL,
   `clue_theme` enum('Terror','Fiction','Fantasy') NOT NULL,
   `clue_level` enum('Easy','Intermediate','Hard') NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE `clues` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `decoration_items` (
-  `decoration_item_id` int(11) NOT NULL,
+  `decoration_item_id` int(11) UNIQUE NOT NULL,
   `decoration_item_name` varchar(20) NOT NULL,
   `decoration_item_description` varchar(200) NOT NULL,
   `decoration_item_theme` enum('Terror','Fiction','Fantasy') NOT NULL,
@@ -40,15 +40,15 @@ CREATE TABLE `game_sessions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `notifications` (
-  `notification_id` int(11) NOT NULL,
+  `notification_id` int(11) UNIQUE NOT NULL,
   `notification_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `notification_type` enum('Generic','Personal') DEFAULT NULL,
   `notification_description` varchar(200) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) UNIQUE NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `payments` (
-  `payment_id` int(11) NOT NULL,
+  `payment_id` int(11) UNIQUE NOT NULL,
   `payment_mode` enum('Credit card','Bizum','PayPal') DEFAULT NULL,
   `payment_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `payment_price` int(11) NOT NULL,
@@ -56,7 +56,7 @@ CREATE TABLE `payments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `rooms` (
-  `room_id` int(11) NOT NULL,
+  `room_id` int(11) UNIQUE NOT NULL,
   `room_name` varchar(100) NOT NULL,
   `room_theme` enum('Terror','Fiction','Fantasy') NOT NULL,
   `room_level` enum('Easy','Intermediate','Hard') NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE `rooms` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) UNIQUE NOT NULL,
   `user_name` varchar(30) NOT NULL,
   `user_surname` varchar(50) NOT NULL,
   `user_address_street` varchar(100) DEFAULT NULL,
@@ -122,11 +122,11 @@ ALTER TABLE `clues`
   ADD CONSTRAINT `fk_clues_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
 
 ALTER TABLE `decoration_items`
-  ADD CONSTRAINT `fk_room_decoration` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
-  ADD CONSTRAINT `fk_clue_decoration` FOREIGN KEY (`clue_id`) REFERENCES `clues` (`clue_id`) UNIQUE;
+  ADD CONSTRAINT `fk_room_decoration` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`),
+  ADD CONSTRAINT `fk_clue_decoration` FOREIGN KEY (`clue_id`) REFERENCES `clues` (`clue_id`);
 
 ALTER TABLE `game_sessions`
-  ADD CONSTRAINT `fk_game_payment` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`payment_id`) UNIQUE,
+  ADD CONSTRAINT `fk_game_payment` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`payment_id`),
   ADD CONSTRAINT `fk_game_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`),
   ADD CONSTRAINT `fk_game_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
