f08b9a7aaae78838cac09b614b48f348b8734da6
Alescu <albert.escudero@gmail.com>
2025-04-02 11:16:23 +0200

    merge esquema sql

--- escape_room.sql
+++ escape_room.sql
@@ -39,17 +39,6 @@ CREATE TABLE `game_sessions` (
   `payment_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `notifications` (
-  `notification_id` int(11) NOT NULL,
-  `notification_title` varchar(50) NOT NULL,
-  `notification_short_description` varchar(100) NOT NULL,
-  `notification_message` varchar(500) NOT NULL,
-  `notification_type` enum('GENERIC','PERSONAL','SERVICE','PAYMENT','GAME') DEFAULT NULL,
-  `notification_date_reg` timestamp NOT NULL DEFAULT current_timestamp(),
-  `notification_date_modify` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 CREATE TABLE `payments` (
   `payment_id` int(11) NOT NULL,
   `payment_mode` enum('Credit card','Bizum','PayPal') DEFAULT NULL,
@@ -86,23 +75,72 @@ CREATE TABLE `rooms` (
   `room_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `user_nick_name` varchar(20) NOT NULL,
-  `user_name` varchar(30) NOT NULL,
-  `user_surname` varchar(100) NOT NULL,
-  `user_mail` varchar(50) DEFAULT NULL,
-  `user_idCard` varchar(100) DEFAULT NULL,
-  `user_address_street` varchar(100) DEFAULT NULL,
-  `user_address_number` smallint(5) UNSIGNED DEFAULT NULL,
-  `user_address_floor` varchar(10) DEFAULT NULL,
-  `user_address_door` varchar(10) DEFAULT NULL,
-  `user_city` varchar(30) DEFAULT NULL,
-  `user_zip_code` varchar(20) DEFAULT NULL,
-  `user_country` varchar(150) DEFAULT NULL,
-  `user_phone` varchar(50) DEFAULT NULL
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `notification_title` varchar(50) NOT NULL,
+  `notification_short_description` varchar(100) NOT NULL,
+  `notification_message` varchar(500) NOT NULL,  
+  `notification_shipping_type` enum('SMS','EMAIL') DEFAULT NULL,
+  `notification_type` enum('GENERIC','PERSONAL','SERVICE','PAYMENT','GAME') DEFAULT NULL,
+  `notification_date_reg` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `notification_date_modify` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `notifications_user` (
+	`notification_user_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`notification_id` int(11) NOT NULL,
+	`user_id` int(11),
+	`notification_register_status` enum('SENDED','PENDING','CANCELED') DEFAULT NULL,
+	`notification_register_date_reg` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	`notification_register_date_modify` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `notifications_game` (
+	`notification_register_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`notification_id` int(11) NOT NULL,
+	`game_id` int(11),
+	`notification_register_status` enum('SENDED','PENDING','CANCELED') DEFAULT NULL,
+	`notification_register_date_reg` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	`notification_register_date_modify` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `generic_notifications_register` (
+	`notification_register_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`notification_id` int(11) NOT NULL,	
+	`notification_register_level` enum('Informative ','Important ','Critical ') DEFAULT NULL,
+	`notification_register_status` enum('SENDED','PENDING','CANCELED') DEFAULT NULL,
+	`notification_register_date_reg` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	`notification_register_date_modify` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_nick_name` varchar(20) UNIQUE NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `user_surname` varchar(100) NOT NULL,
+  `user_mail` varchar(50) UNIQUE,
+  `user_idCard` varchar(100),
+  `user_address_street` varchar(100),
+  `user_address_number` smallint UNSIGNED,
+  `user_address_floor` varchar(10),
+  `user_address_door` varchar(10),
+  `user_city` varchar(30),
+  `user_zip_code` varchar(20),
+  `user_country` varchar(150),
+  `user_phone` varchar(50),
+  INDEX (user_mail),
+  INDEX (user_nick_name),
+  INDEX (user_surname),
+  INDEX (user_idCard)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `user_pssw` (
+  `user_pssw_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_pssw_code` varchar(256) NOT NULL,
+  `user_pssw_date_reg` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `user_pssw_date_modify` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE `clues`
   ADD PRIMARY KEY (`clue_id`);
@@ -115,10 +153,6 @@ ALTER TABLE `game_sessions`
   ADD PRIMARY KEY (`game_id`),
   ADD KEY `fk_game_room` (`room_id`),
   ADD KEY `fk_game_payment` (`payment_id`);
-
-ALTER TABLE `notifications`
-  ADD PRIMARY KEY (`notification_id`),
-  ADD KEY `fk_notification_user` (`user_id`);
 
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`payment_id`),
@@ -141,17 +175,6 @@ ALTER TABLE `relation_user_game`
 
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`room_id`);
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_nick_name` (`user_nick_name`),
-  ADD UNIQUE KEY `user_mail` (`user_mail`),
-  ADD KEY `user_mail_2` (`user_mail`),
-  ADD KEY `user_nick_name_2` (`user_nick_name`),
-  ADD KEY `user_surname` (`user_surname`),
-  ADD KEY `user_idCard` (`user_idCard`);
-
-
 ALTER TABLE `clues`
   MODIFY `clue_id` int(11) NOT NULL AUTO_INCREMENT;
 
@@ -160,9 +183,6 @@ ALTER TABLE `decoration_items`
 
 ALTER TABLE `game_sessions`
   MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `notifications`
-  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `payments`
   MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
@@ -179,19 +199,12 @@ ALTER TABLE `relation_user_game`
 ALTER TABLE `rooms`
   MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
-
 ALTER TABLE `decoration_items`
   ADD CONSTRAINT `fk_clue_decoration` FOREIGN KEY (`clue_id`) REFERENCES `clues` (`clue_id`);
 
 ALTER TABLE `game_sessions`
   ADD CONSTRAINT `fk_game_payment` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`payment_id`),
   ADD CONSTRAINT `fk_game_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
-
-ALTER TABLE `notifications`
-  ADD CONSTRAINT `fk_notification_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 ALTER TABLE `payments`
   ADD CONSTRAINT `fk_payment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
@@ -207,4 +220,19 @@ ALTER TABLE `relation_decoration_item_room`
 ALTER TABLE `relation_user_game`
   ADD CONSTRAINT `fk_game_id` FOREIGN KEY (`game_id`) REFERENCES `game_sessions` (`game_id`),
   ADD CONSTRAINT `fk_usergame` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  
+ALTER TABLE `notifications_user`
+  ADD CONSTRAINT `fk_notifications_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `fk_notifications_user_notif` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`notification_id`);
+
+ALTER TABLE `notifications_game`
+  ADD CONSTRAINT `fk_notifications_game` FOREIGN KEY (`game_id`) REFERENCES `game_sessions` (`game_id`),
+  ADD CONSTRAINT `fk_notifications_game_notif` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`notification_id`);
+
+ALTER TABLE `generic_notifications_register`
+  ADD CONSTRAINT `fk_generic_notifications_register` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`notification_id`);
+  
+ALTER TABLE `user_pssw`
+  ADD CONSTRAINT `fk_user_pssw` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+ 
 COMMIT;
